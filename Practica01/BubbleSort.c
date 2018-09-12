b90dc445d5a78b86d829a3f08ba3b6cacd10b110
rockorwr <dams_rive@ciencias.unam.mx>
2018-09-12 18:22:12 -0500

    Ya está listo el calculo de la fecha 2038, falta pulir el BubbleSort

--- Practica01/BubbleSort.c
+++ Practica01/BubbleSort.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-/*
-int main(int argc, char const *argv[])
-{
-	return 0;
-}*/
-
-#include <stdio.h>
+#include <limits.h>
  
 void swap(int *xp, int *yp)
 {
@@ -33,69 +27,56 @@ void printArray(int arr[], int size)
 // A function to implement bubble sort
 void bubbleSort(int arr[], int arrD[],int n)
 {
-
    int i, j;
    for (i = 0; i < n-1; i++)      
- 
        // Last i elements are already in place   
        for (j = 0; j < n-i-1; j++) 
            if (arr[j] > arr[j+1])
            {	              	
               	swap(&arr[j], &arr[j+1]);
               	swap(&arrD[j], &arrD[j+1]);
-              	printArray(arrD, n);//Cada que se haga un Swap imprimimos el arreglo            	
+              	//printArray(arrD, n);//Cada que se haga un Swap imprimimos el arreglo            	
            }
 }
  
-void segundosRestantes()
+void segundosRestantes2038(void)
 {
-    time_t actual;
-    time(&actual);
-    printf("Difference is  %.0f seconds\n",difftime(2147483647, actual));
+    time_t actual = time(NULL);
+    printf("Segundos Restantes para la fecha: %.0f s\n",difftime(INT_MAX, actual));    
 }
 
+void fechaExacta2038(void)
+{
+	time_t final = INT_MAX; 
+    printf("Local Time\t%s", asctime(localtime(&final)));
+    printf("Coordinated Universal Time (UTC)\t%s", asctime(gmtime(&final)));
+}
  
-// Driver program to test above functions
 int main()
 {
-	/*
+	
 	int arrD[7];
     int arr[] = {64, 34, 25, 12, 22, 11, 90};
     int *puntero;
     for (int i = 0; i < 7; ++i)
     {
     	puntero = &arr[i];
-    	printf("%p\n", puntero);
+    	printf("%p\t", puntero);
     	arrD[i] = (int)puntero;
     }
     int n = sizeof(arr)/sizeof(arr[0]);
-    printf("Unsorted array: \n");
+    printf("Imprimimos el arreglo: \n");
     printArray(arr, n);
     printArray(arrD, n);
     bubbleSort(arr,arrD, n);
-    printf("Sorted array: \n");
+    printf("Imprimimos el arreglo: \n");
     printArray(arr, n);
-    
+    printArray(arrD, n);
 
-	time_t t = time(NULL);
-	struct tm *tm = localtime(&t);
-	char fechayhora[100];
-
-	strftime(fechayhora, 100, "%s", tm);
-	printf ("Hoy es: %s\n", fechayhora);
-
-	int tiempo = atoi(fechayhora);
-
-    time_t epoch = tiempo;//2147483647
-    printf("%ld seconds since the epoch began\n", (long)epoch);
-    printf("%s", asctime(gmtime(&epoch)));
-
-    int x = 2147483647 - tiempo;
-    int dias = x/86400;
-    printf("Segundos faltantes %d\n", x);
-    printf("Días faltantes %d\n", dias);
-	*/
-    segundosRestantes();
+/*
+    segundosRestantes2038();
+    fechaExacta2038();
+*/
+ 
     return 0;
-
 }
